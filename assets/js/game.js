7ce26345194f940897e50316dc73c4011fe58f6b
Sean Murphy <seanmurphy@Seans-MacBook-Pro.local>
2021-08-03 15:11:38 -0400

    3.2.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,16 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(enemyNames[i] + " is at " + i + "index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -69,4 +78,4 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 };
 
 // run fight function to start game
-fight();
+// fight();
